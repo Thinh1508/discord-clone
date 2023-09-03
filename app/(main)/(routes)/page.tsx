0738059a384735eb29480265b8cbfa8db1433584
Kuro1508 <thinhhuynh115@gmail.com>
2023-09-03 23:13:47 +0700

    authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
+
 export default function Home() {
-  return <div>this is protected routes</div>
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  )
 }
