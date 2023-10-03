f1bf411718fd9887e735981fbd22c49956ae82f0
Thinhhuhydss <thinhhuynh115@gmail.com>
2023-10-03 23:00:56 +0700

    emoji bar

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -6,9 +6,12 @@ import qs from "query-string"
 import { Plus, SendHorizonal, Smile } from "lucide-react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
 
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useModal } from "@/hooks/use-modal-store"
+import { EmojiPicker } from "@/components/emoji-picker"
 
 interface ChatInputProps {
   apiUrl: string
@@ -22,6 +25,9 @@ const fromSchema = z.object({
 })
 
 export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
+  const { onOpen } = useModal()
+  const router = useRouter()
+
   const form = useForm<z.infer<typeof fromSchema>>({
     resolver: zodResolver(fromSchema),
     defaultValues: {
@@ -39,6 +45,8 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
       })
 
       axios.post(url, values)
+      form.reset()
+      router.refresh()
     } catch (error) {
       console.log(error)
     }
@@ -54,27 +62,31 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
             <FormItem>
               <FormControl>
                 <div className="relative p-4 pb-6">
-                  <button
-                    type="button"
-                    onClick={() => {}}
-                    className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
-                  >
-                    <Plus className="text-white dark:text-[#313338]" />
-                  </button>
+                  <div className="absolute top-7 left-8 flex items-center justify-center gap-x-2">
+                    <button
+                      type="button"
+                      onClick={() => onOpen("messageFile", { apiUrl, query })}
+                      className="h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
+                    >
+                      <Plus className="text-white dark:text-[#313338]" />
+                    </button>
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value}${emoji}`)
+                      }
+                    />
+                  </div>
                   <Input
                     disabled={isLoading}
-                    className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                    className="pl-20 pr-12 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
                     placeholder={`Message ${
                       type === "conversation" ? name : "#" + name
                     }`}
                     {...field}
                   />
-                  <div className="absolute top-7 right-8 flex items-center justify-center gap-x-2">
-                    <button type="submit">
-                      <SendHorizonal />
-                    </button>
-                    <Smile />
-                  </div>
+                  <button type="submit" className="absolute top-7 right-8">
+                    <SendHorizonal className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+                  </button>
                 </div>
               </FormControl>
             </FormItem>
